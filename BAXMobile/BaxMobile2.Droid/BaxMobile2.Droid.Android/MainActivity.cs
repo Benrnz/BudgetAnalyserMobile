01bf527cc2f3751ccafd1639c92be8e89ffdc96f
Ben Rees <brees@fraedom.com>
2019-02-09 11:22:27 +1300

    Borked references too broken to fix.  Creating new app shell project.

---
+++ BAXMobile/BaxMobile2.Droid/BaxMobile2.Droid.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using BAXMobile;
+
+namespace BaxMobile2.Droid
+{
+    [Activity(Label = "BaxMobile2.Droid", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
