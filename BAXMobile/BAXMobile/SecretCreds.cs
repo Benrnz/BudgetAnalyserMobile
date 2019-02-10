b93a2b86843bf3bea06d2fbaa7dd1be7f59a55b8
Ben Rees <brees@fraedom.com>
2019-02-10 16:17:22 +1300

    Remove SecretCreds file.  (Keys cycled)

--- BAXMobile/BAXMobile/SecretCreds.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BAXMobile
-{
-    public static class SecretCreds
-    {
-        public const string AwsAccessKeyId = "AKIAIKOKRXRFV7CJPIJA";
-        public const string AwsSecret = "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";
-    }
-}
