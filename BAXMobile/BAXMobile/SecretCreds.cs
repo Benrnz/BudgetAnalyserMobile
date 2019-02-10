909b5ea71a7ffa1319e8e2f1fb214e0c99ff4ca7
Ben Rees <brees@fraedom.com>
2019-02-10 15:58:23 +1300

    Folders fully cleaned up and simplified.

---
+++ BAXMobile/BAXMobile/SecretCreds.cs
@@ -0,0 +1,8 @@
+﻿namespace BAXMobile
+{
+    public static class SecretCreds
+    {
+        public const string AwsAccessKeyId = "AKIAIKOKRXRFV7CJPIJA";
+        public const string AwsSecret = "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";
+    }
+}
