e86591372585d2c5bf1281ea67282e06900fc6c0
Ben Rees <ben@rees.biz>
2023-03-18 21:28:51 +1300

    Remove old outdated projects
    These projects were unable to be updated.  Created a set of clean new projects instead and migrated code in.

--- BAXMobile/BAXMobile2/SecretCreds2.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BAXMobile
-{
-    public static class SecretCreds
-    {
-        public const string AwsAccessKeyId = "AKIAWHEW6QQSAXD7QOOL";
-        public const string AwsSecret = "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";
-    }
-}
